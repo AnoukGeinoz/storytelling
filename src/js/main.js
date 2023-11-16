ec9a6aef12369fa85c9445d5cd81729e7457a71b
AnoukGeinoz <Anouk.Geinoz@studentfr.ch>
2023-11-16 11:17:25 +0100

    realisation bouton declencheur

--- src/js/main.js
+++ src/js/main.js
@@ -30,29 +30,47 @@ tl.to(".l-section-horizontal", {
   x: amplitudeX + "%",
   ease: "sine.inOut",
 });
-tl.to(".l-section-horizontal-2", {
-  scrollTrigger: {
-    trigger: ".slider-mask-2",
-    start: "top top",
-    end: "+=300%",
-    pin: true,
-    scrub: true,
-    // markers: true,
-    repeat: 3,
-  },
-  x: amplitudeX + "%",
-  ease: "sine.inOut",
+
+var slider1Button = document.querySelector("#slider-1-button");
+
+slider1Button.addEventListener("click", function () {
+  document.querySelector(".slider-mask-2").classList.remove("hidden");
+  tl.to(".l-section-horizontal-2", {
+    scrollTrigger: {
+      trigger: ".slider-mask-2",
+      start: "top top",
+      end: "+=300%",
+      pin: true,
+      scrub: true,
+      // markers: true,
+      repeat: 3,
+    },
+    x: amplitudeX + "%",
+    ease: "sine.inOut",
+  });
 });
-tl.to(".l-section-horizontal-3", {
-  scrollTrigger: {
-    trigger: ".slider-mask-3",
-    start: "top top",
-    end: "+=300%",
-    pin: true,
-    scrub: true,
-    // markers: true,
-    repeat: 3,
-  },
-  x: amplitudeX + "%",
-  ease: "sine.inOut",
+
+var slider2Button = document.querySelector("#slider-2-button");
+
+slider2Button.addEventListener("click", function () {
+  document.querySelector(".slider-mask-3").classList.remove("hidden");
+  tl.to(".l-section-horizontal-3", {
+    scrollTrigger: {
+      trigger: ".slider-mask-3",
+      start: "top top",
+      end: "+=300%",
+      pin: true,
+      scrub: true,
+      // markers: true,
+      repeat: 3,
+    },
+    x: amplitudeX + "%",
+    ease: "sine.inOut",
+  });
+});
+
+var slider3Button = document.querySelector("#slider-3-button");
+
+slider3Button.addEventListener("click", function () {
+  document.querySelector(".shop").classList.remove("hidden");
 });
