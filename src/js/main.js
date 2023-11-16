6fcbb0bf9b2729109bad23c648d4be439f706907
AnoukGeinoz <Anouk.Geinoz@studentfr.ch>
2023-11-16 09:21:09 +0100

    ajout du scrolltrigger

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,57 @@
 const n = 3;
 var amplitudeX = (-100 * (n - 1)) / n;
 
-gsap.to(".l-section-horizontal", {
+// gsap.to(".l-section-horizontal", {
+//   scrollTrigger: {
+//     trigger: ".slider-mask",
+//     start: "top top",
+//     end: "+=300%",
+//     pin: true,
+//     scrub: true,
+//     markers: true,
+//     repeat: 3,
+//   },
+//   x: amplitudeX + "%",
+//   ease: "sine.inOut",
+// });
+
+let tl = gsap.timeline({});
+
+tl.to(".l-section-horizontal", {
   scrollTrigger: {
     trigger: ".slider-mask",
     start: "top top",
     end: "+=300%",
     pin: true,
     scrub: true,
-    markers: true,
+    // markers: true,
+    repeat: 3,
+  },
+  x: amplitudeX + "%",
+  ease: "sine.inOut",
+});
+tl.to(".l-section-horizontal-2", {
+  scrollTrigger: {
+    trigger: ".slider-mask-2",
+    start: "top top",
+    end: "+=300%",
+    pin: true,
+    scrub: true,
+    // markers: true,
+    repeat: 3,
+  },
+  x: amplitudeX + "%",
+  ease: "sine.inOut",
+});
+tl.to(".l-section-horizontal-3", {
+  scrollTrigger: {
+    trigger: ".slider-mask-3",
+    start: "top top",
+    end: "+=300%",
+    pin: true,
+    scrub: true,
+    // markers: true,
+    repeat: 3,
   },
   x: amplitudeX + "%",
   ease: "sine.inOut",
